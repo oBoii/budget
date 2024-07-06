05e8a724acaa7837ba5b3db2c7e1c3a02d48898a
Anonymous user <fabianbusiness@outlook.com>
2024-07-06 23:16:04 +0200

    create_trip_expenses_table(), create_trips_table()

--- backend/create_db.py
+++ backend/create_db.py
@@ -54,5 +54,48 @@ def create_monthly_expenses_table():
     conn.close()
 
 
+def create_trips_table():
+    # Connect to or create an SQLite database
+    conn = sqlite3.connect("expenses.db")
+    cursor = conn.cursor()
+
+    # Create a table to store trips
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS trips (
+            id INTEGER PRIMARY KEY,
+            description TEXT NOT NULL,
+            start_date date NOT NULL)
+        """)
+    # Commit the changes and close the connection
+    conn.commit()
+    conn.close()
+
+def create_trip_expenses_table():
+    # Connect to or create an SQLite database
+    conn = sqlite3.connect("expenses.db")
+    cursor = conn.cursor()
+
+    # Create a table to store trip expenses
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS trip_expenses (
+            id INTEGER PRIMARY KEY,
+            trip_id INTEGER NOT NULL,
+            date date NOT NULL,
+            price_fabian REAL,
+            price_elisa REAL,
+            paid_by TEXT, -- either "fabian" or "elisa"
+            category TEXT,
+            description TEXT DEFAULT NULL,
+            spread_cost BOOLEAN DEFAULT FALSE -- whether the cost should be spread over the duration of the trip (e.g. accommodation)
+        )
+    """
+    )
+
+    # Commit the changes and close the connection
+    conn.commit()
+    conn.close()
+
 if __name__ == "__main__":
-    create_monthly_expenses_table()
+    create_trip_expenses_table()
